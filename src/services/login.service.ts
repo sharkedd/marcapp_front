cfa355a9dbfbe64700abd12d30fca38b31ec4265
Sharked <jose.carrillo@alumnos.ucn.cl>
2024-05-09 10:21:11 -0400

    prueba

--- src/services/login.service.ts
+++ src/services/login.service.ts
@@ -14,8 +14,8 @@ const loginService = async (payload: { email: string; password: string }) => {
       console.log('Log desde service responde.data = ' + JSON.stringify(response?.data));
       return { success: true, data: response.data };
     } else {
-      console.log('Contraseña incorrecta');
-      return { success: false, message: 'Contraseña incorrecta' };
+      console.log('Credenciales incorrectas');
+      return { success: false, message: 'Credenciales incorrecta' };
     }
   } catch (error: unknown) {
     return { success: false, message: 'Error de servidor' };
