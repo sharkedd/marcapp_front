2553d5a4253142ec967510179e02bf015a68bebc
Sharked <jose.carrillo@alumnos.ucn.cl>
2024-06-14 12:14:03 -0400

    Modificación lógica de obtención de estado

--- src/services/getRegistrationStateService.service.ts
+++ src/services/getRegistrationStateService.service.ts
@@ -1,15 +1,24 @@
 import axios from "axios";
+import moment from "moment";
 
 const getRegistrationStateService = async (idUser: number) => {
   try {
+    const today = moment().format('DD-MM-YYYY');
+    const tomorrow = moment().add(1, 'days').format('DD-MM-YYYY');
+    console.log(today);
+    console.log(tomorrow);
     const endpoint = `${process.env.EXPO_PUBLIC_MS_TIMEREGISTRATION_URL}/marcaje/user/today/${idUser}`;
     console.log(endpoint)
-    const response = await axios.get(endpoint);
-    if(response?.status === 200) {
+    const response = await axios.post(endpoint, {
+      startDate: today, 
+      endDate: tomorrow,
+    });
+    if(response?.status === 201) {
         console.log('Data:', response.data)
         return { success: true, data: response.data};
     } else {
       console.log("Error del servidor")
+      console.log(response.status);
       return { success: false, message: 'Error del servidor' };
     }
   } catch (error: unknown) {
